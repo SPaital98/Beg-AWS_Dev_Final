43458519ffdae19034824be0721cc1d34324ec2a
SPaital98 <apppermis9286@gmail.com>
2026-03-08 12:36:21 +0530

    Update Footer.jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -123,7 +123,7 @@ const Footer = () => {
                   </span>
                   Email:
                 </h6>
-                <p className="mb-0">abc.com</p>
+                <p className="mb-0">abcd.com</p>
               </ListGroupItem>
 
               <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
